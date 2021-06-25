12f466ffdf16da5eafbb5bcf4cd2af92d73bbb55
english721 <78639301+english721@users.noreply.github.com>
2021-06-25 09:31:41 +0900

    deployed version
    toDoListHogwartzVersion 1.0

---
+++ bg.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 9;
+
+
+function paintImage(imgNumber) {
+    const image = new Image();
+    image.src = "https://images.unsplash.com/photo-1491156855053-9cdff72c7f85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80";
+    image.classList.add("bgImage");
+    body.prepend(image);
+}
+
+function genRandom(){
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = genRandom();
+    paintImage(randomNumber);
+}
+
+
+init();
