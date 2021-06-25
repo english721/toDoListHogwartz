12c56e909236ea7c2a0edcab774bd6348b531744
english721 <78639301+english721@users.noreply.github.com>
2021-06-25 13:51:53 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -5,7 +5,7 @@ const IMG_NUMBER = 9;
 
 function paintImage(imgNumber) {
     const image = new Image();
-    image.src = "https://images.unsplash.com/photo-1491156855053-9cdff72c7f85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80";
+    image.src = "https://images.unsplash.com/photo-1533462392871-5a6c02e7132d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80";
     image.classList.add("bgImage");
     body.prepend(image);
 }
